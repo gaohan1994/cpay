840ed18d1fee28a6b5602c6b6171b398538d04f0
gaohan1994 <871418277@qq.com>
2020-08-12 10:11:39 +0800

    feat(模块1): 终端应用管理
    终端应用管理表单页面

---
+++ src/pages/terminal/application/types/index.ts
@@ -0,0 +1,11 @@
+export declare module FetchField {
+  interface ITerminalAppList {
+    appCode: string;
+    appName: string;
+    type: string;
+    usedDuration: string;
+    usedNum: string;
+    versionCode: string;
+    versionName: string;
+  }
+}
