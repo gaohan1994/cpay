b70f672caa30d81fece02a77ea430741b1502274
gaohan1994 <871418277@qq.com>
2020-08-21 17:56:44 +0800

    feat(模块1): 准备合并
    加入终端详情，包括5个tab，其中当月流量的表头等待合并之后使用date组件

--- src/pages/terminal/group/index.tsx
+++ src/pages/terminal/group/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Form, Table } from 'antd';
-import { useAntdTable } from 'ahooks';
+import { Form, Table, Input, Col } from 'antd';
+import { useAntdTable, useBoolean } from 'ahooks';
 import { PaginatedParams } from 'ahooks/lib/useAntdTable';
 import { PlusOutlined } from '@ant-design/icons';
 import { terminalGroupList } from './constants';
@@ -10,10 +10,15 @@ import { FormItem, FormItmeType } from '@/component/form/type';
 import Forms from '@/component/form';
 import { useStore } from '@/pages/common/costom-hooks';
 import { formatPaginate } from '@/common/request-util';
+import Modal from 'antd/lib/modal/Modal';
 
+const { Item } = Form;
+const { TextArea } = Input;
 export default () => {
   useStore([]);
+  const [modalVisible, { setFalse, setTrue, toggle }] = useBoolean(false);
   const [form] = Form.useForm();
+  const [modalForm] = Form.useForm();
   const { tableProps, search }: any = useAntdTable(
     (paginatedParams: PaginatedParams[0], tableProps: any) => {
       return terminalGroupList({
@@ -76,6 +81,7 @@ export default () => {
       title: '新增',
       type: 'primary',
       icon: <PlusOutlined />,
+      onClick: setTrue,
     },
   ];
 
@@ -91,6 +97,57 @@ export default () => {
         }}
       />
       <Table columns={columns} {...tableProps} />
+      <Modal
+        title="终端组别新增"
+        cancelText="取消"
+        okText="确定"
+        visible={modalVisible}
+        maskClosable
+        // onOk={handleOk}
+        // confirmLoading={confirmLoading}
+        onCancel={setFalse}
+      >
+        <Form form={modalForm} labelCol={{ span: 4 }} wrapperCol={{ span: 16 }}>
+          <Item
+            label="组别名称"
+            name="name"
+            rules={[
+              {
+                required: true,
+                message: '请输入组别名称',
+              },
+            ]}
+          >
+            <Input />
+          </Item>
+          <Item
+            label="类型名称"
+            name="typeName"
+            rules={[
+              {
+                required: true,
+                message: '请输入类型名称',
+              },
+            ]}
+          >
+            <Input />
+          </Item>
+          <Item
+            label="应用图标"
+            name="typeIcon"
+            rules={[
+              {
+                required: true,
+                message: '请上传应用图标',
+              },
+            ]}
+          >
+            <Col>
+              <TextArea />
+            </Col>
+          </Item>
+        </Form>
+      </Modal>
     </div>
   );
 };
